38e9143e93bfd2b834304d6c9ed89643481c44e2
Ricky Lu <real0st@gmail.com>
2018-06-05 15:34:02 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+mha_campaign site
+
+In the manga/anime Boku No Hero Academia, the students in the series had to elect a class representative.
+This stie is my attempt of creating a fake campaign website for some of the characters in the series with some humor added (and also for me to personally learn how to build a website using HTML, CSS, JS, Node, Bootstrap, etc..)
+
+
---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/mha_campaign");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+
+app.get("/", function(req, res){
+   res.render("landing")
+});
+
+app.get("/about", function(req, res){
+   res.send("You have reached about me page.")
+});
+
+app.get("/blog", function(req, res){
+   res.send("You have reached blog page.")
+});
+
+app.get("/contact", function(req, res){
+   res.send("You have reached Contact Me page.")
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Website has started.");
+});
